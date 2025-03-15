25d6f330c980ea371c8627545352f1218c8b5c69
Chris McCormick <chris@mccormick.cx>
2025-03-15 21:16:06 +0800

    Tests for prod mode.

--- tests.cljs
+++ tests.cljs
@@ -7,7 +7,8 @@
     ["child_process" :as child-process]
     [clojure.test :refer [deftest async is] :as t]
     [promesa.core :as p]
-    [applied-science.js-interop :as j]))
+    [applied-science.js-interop :as j]
+    [clojure.tools.cli :as cli]))
 
 (t/use-fixtures
   :once
@@ -49,20 +50,38 @@
                             (js/setTimeout check-server 100))))))]
           (check-server))))))
 
+(def cli-options
+  [["-d" "--dir DIR" "Path to dir to serve."
+    :default "./"]
+   ["-p" "--port PORT" "Webserver port number."
+    :default 8123
+    :parse-fn js/Number]
+   [nil "--prod" "Run in production mode"]])
+
 (defn start-josh-server 
   "Start a josh server in a subprocess with the given options"
-  [{:keys [dir port]
-    :or {dir "./" port 8123}}]
-  (p/let [server-process (child-process/spawn 
-                           "npx" 
-                           #js ["nbb" "josh.cljs" 
-                                "--dir" dir 
-                                "--port" (str port)]
-                           #js {:stdio "inherit"})]
-    (js/console.log (str "Started josh server subprocess on port " port))
+  [args env-vars]
+  (let [parsed-opts (:options (cli/parse-opts args cli-options))
+        port (:port parsed-opts)
+        ; Create a proper environment by preserving the current process.env
+        ; and merging in any custom environment variables
+        current-env (js/Object.assign #js {} (.-env js/process))
+        env-obj (if env-vars
+                  (js/Object.assign current-env (clj->js env-vars))
+                  current-env)
+        cmd-args (clj->js (concat ["nbb" "josh.cljs"] args))
+        server-process (child-process/spawn 
+                         "npx" 
+                         cmd-args
+                         #js {:stdio "inherit"
+                              :env env-obj})]
+    (js/console.log (str "Started josh server subprocess with args: " 
+                         (js/JSON.stringify cmd-args)
+                         (when (and env-vars (.-NODE_ENV env-vars))
+                           (str " with NODE_ENV=" (.-NODE_ENV env-vars)))))
     ; Wait for server to be ready (max 5 seconds)
-    (wait-for-server-ready port 5000)
-    server-process))
+    (p/let [_ (wait-for-server-ready port 5000)]
+      server-process)))
 
 (defn stop-josh-server 
   "Stop a running josh server process"
@@ -75,7 +94,7 @@
   (t/testing "First basic server tests"
     (async
       done
-      (p/let [server-process (start-josh-server {:dir "example" :port 8123})
+      (p/let [server-process (start-josh-server ["--dir" "example" "--port" "8123"] nil)
               res (js/fetch "http://localhost:8123/")
               response (.text res)]
         
@@ -91,7 +110,7 @@
   (t/testing "HTML loader script injection"
     (async
       done
-      (p/let [server-process (start-josh-server {:dir "example" :port 8124})
+      (p/let [server-process (start-josh-server ["--dir" "example" "--port" "8124"] nil)
               res (js/fetch "http://localhost:8124/")
               html-content (.text res)]
         
@@ -104,6 +123,44 @@
             "Response should contain the CLJS reload function")
         (is (.includes html-content "reload-css-tags") 
             "Response should contain the CSS reload function")
+        
+        (stop-josh-server server-process)
+        
+        (done)))))
+
+(deftest prod-flag-test
+  (t/testing "Production mode with --prod flag"
+    (async
+      done
+      (p/let [server-process (start-josh-server ["--dir" "example" "--port" "8125" "--prod"] nil)
+              res (js/fetch "http://localhost:8125/")
+              html-content (.text res)]
+        
+        ; Check that the loader script is NOT injected
+        (is (not (.includes html-content "_josh-reloader")) 
+            "Response should NOT contain the loader script")
+        (is (not (.includes html-content "setup-sse-connection")) 
+            "Response should NOT contain the SSE connection setup")
+        
+        (stop-josh-server server-process)
+        
+        (done)))))
+
+(deftest prod-env-test
+  (t/testing "Production mode with NODE_ENV=production"
+    (async
+      done
+      (p/let [server-process (start-josh-server 
+                               ["--dir" "example" "--port" "8126"]
+                               {"NODE_ENV" "production"})
+              res (js/fetch "http://localhost:8126/")
+              html-content (.text res)]
+        
+        ; Check that the loader script is NOT injected
+        (is (not (.includes html-content "_josh-reloader")) 
+            "Response should NOT contain the loader script")
+        (is (not (.includes html-content "setup-sse-connection")) 
+            "Response should NOT contain the SSE connection setup")
         
         (stop-josh-server server-process)
         
