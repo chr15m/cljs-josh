dbe853c8725e35cd092bab03e3e936f1e712e75b
Chris McCormick <chris@mccormick.cx>
2025-08-01 17:19:04 +0800

    Basic messages going back and forth.

--- josh.cljs
+++ josh.cljs
@@ -5,7 +5,7 @@
     [clojure.edn :as edn]
     [clojure.string :as str]
     [clojure.tools.cli :as cli]
-    ["bencode" :as bencode]
+    ["bencode$default" :as bencode]
     ["net" :as net]
     ["ws" :refer [WebSocketServer]]
     ["os" :as os]
@@ -83,51 +83,101 @@
 
 (defonce nrepl-ws-channel (atom nil))
 (defonce nrepl-sessions (atom {}))
+(defonce nrepl-sockets (atom #{}))
 (defonce ws-port (atom nil))
 
 (defn- send-bencode [out response]
+  (js/console.log "nREPL out:" response)
   (.write out (bencode/encode response)))
 
 (defn- forward-to-browser [socket msg-clj]
+  (js/console.log "nREPL -> browser:" (clj->js msg-clj))
   (if-let [ws @nrepl-ws-channel]
     (.send ws (pr-str msg-clj))
-    (let [response (clj->js {:status ["error" "done"]
-                             :err "No browser connected."
-                             :id (:id msg-clj)
-                             :session (:session msg-clj)})]
+    (let [session-id (:session msg-clj)
+          id (:id msg-clj)
+          err-msg "#error {:message \"No browser connected.\"}"
+          ex-resp (cond-> {:ex err-msg :id id}
+                    session-id (assoc :session session-id))
+          ns-resp (cond-> {:ns "user" :id id}
+                    session-id (assoc :session session-id))
+          status-resp (cond-> {:status ["done"] :id id :ns ""}
+                        session-id (assoc :session session-id))]
       (js/console.error "nREPL: No browser connected, sending error to client.")
-      (send-bencode socket response))))
+      (send-bencode socket (clj->js ex-resp))
+      (send-bencode socket (clj->js ns-resp))
+      (send-bencode socket (clj->js status-resp)))))
+
+(defn- do-eval [socket msg-clj]
+  #_ (forward-to-browser socket (assoc msg-clj :op :eval))
+  (let [code (:code msg-clj)
+        session-id (:session msg-clj)
+        id (:id msg-clj)]
+    (if (and code (or (str/includes? code "clojure.main/repl-requires")
+                      (str/includes? code "System/getProperty")))
+      (let [value-resp (cond-> {:value "nil" :id id}
+                         session-id (assoc :session session-id))
+            status-resp (cond-> {:status ["done"] :id id}
+                          session-id (assoc :session session-id))]
+        (js/console.log "nREPL: Intercepted clojure.main/repl-requires or System/getProperty")
+        (send-bencode socket (clj->js value-resp))
+        (send-bencode socket (clj->js status-resp)))
+      (forward-to-browser socket (assoc msg-clj :op :eval)))))
 
 (defn- handle-nrepl-message [socket msg]
   (let [msg-clj (js->clj msg :keywordize-keys true)
-        op (:op msg-clj)]
+        op (keyword (:op msg-clj))]
+    (js/console.log "nREPL in:" (clj->js msg-clj))
     (case op
-      "clone" (let [session-id (str (random-uuid))
-                    response (clj->js {:new-session session-id
-                                       :status ["done"]
-                                       :id (:id msg-clj)})]
-                (swap! nrepl-sessions assoc session-id socket)
-                (send-bencode socket response))
+      :clone (let [session-id (str (random-uuid))
+                   response (clj->js {:new-session session-id
+                                      :status ["done"]
+                                      :id (:id msg-clj)})]
+               (swap! nrepl-sessions assoc session-id socket)
+               (send-bencode socket response))
 
-      "describe" (let [response (clj->js {:ops {"clone" {} "describe" {} "eval" {} "load-file" {}}
-                                           :versions {"josh" "0.0.1"
-                                                      "nrepl" "0.9.0"}
-                                           :status ["done"]
-                                           :id (:id msg-clj)})]
-                   (send-bencode socket response))
+      :describe (let [node-version (j/get-in js/process [:versions :node])
+                      [major minor incremental] (str/split node-version #"\.")
+                      response
+                      (clj->js
+                        {:versions {"nbb-nrepl" {"major" "TODO"
+                                                 "version-string" "TODO"}
+                                    "node" {"major" (str "v" major)
+                                            "minor" minor
+                                            "incremental" incremental
+                                            "version-string"
+                                            (str "v" node-version)}}
+                         :ops (zipmap
+                                (map name [;:classpath
+                                           :clone
+                                           :close
+                                           :complete
+                                           :describe
+                                           :eldo
+                                           :eval
+                                           :info
+                                           :load-file
+                                           :lookup
+                                           :macroexpand])
+                                (repeat {}))
+                         :aux {}
+                         :status ["done"]
+                         :id (:id msg-clj)})]
+                  (send-bencode socket response))
 
-      "eval" (if (or (str/includes? (:code msg-clj "") "clojure.main/repl-requires")
-                     (str/includes? (:code msg-clj "") "System/getProperty"))
-               (let [session-id (:session msg-clj)]
-                 (send-bencode socket (clj->js {:value "nil", :id (:id msg-clj), :session session-id}))
-                 (send-bencode socket (clj->js {:status ["done"], :id (:id msg-clj), :session session-id})))
-               (forward-to-browser socket msg-clj))
+      :eval (do-eval socket msg-clj)
+
+      :load-file (do-eval socket (assoc msg-clj :code (:file msg-clj)))
+
+      (:classpath :macroexpand) (forward-to-browser socket msg-clj)
 
       (forward-to-browser socket msg-clj))))
 
 (defn- handle-nrepl-client [socket]
+  (swap! nrepl-sockets conj socket)
   (.on socket "close" (fn []
                         (js/console.log "nREPL client disconnected.")
+                        (swap! nrepl-sockets disj socket)
                         (let [to-remove (->> @nrepl-sessions
                                              (filter (fn [[_v s]] (= s socket)))
                                              (map first))]
@@ -135,20 +185,24 @@
   (let [buffer (atom (js/Buffer.alloc 0))]
     (.on socket "data"
          (fn [data]
+           (js/console.log "socket data:" (.toString data))
            (swap! buffer #(js/Buffer.concat (clj->js [% data])))
-           (loop [continue? true]
-             (when (and continue? (> (.-length @buffer) 0))
-               (let [decoded (try
-                               (let [msg (bencode/decode @buffer)
-                                     bytes (j/get bencode/decode "bytes")]
-                                 {:msg msg :bytes bytes})
-                               (catch js/Error _e nil))]
-                 (if decoded
-                   (do
-                     (handle-nrepl-message socket (:msg decoded))
-                     (swap! buffer #(.slice % (:bytes decoded)))
-                     (recur true))
-                   (recur false)))))))))
+           (loop []
+             (when (> (.-length @buffer) 0)
+               (let [msg (try
+                           (bencode/decode @buffer "utf8")
+                           (catch js/Error e
+                             (when-not (or (str/includes? (.-message e) "Unexpected end of buffer")
+                                           (str/includes? (.-message e) "Not a string")
+                                           (str/includes? (.-message e) "Invalid data"))
+                               (js/console.error "nREPL stream error, closing connection:" e)
+                               (.end socket))
+                             :decode-error))]
+                 (when (not= msg :decode-error)
+                   (let [bytes-consumed (bencode/encodingLength msg)]
+                     (handle-nrepl-message socket msg)
+                     (swap! buffer #(.slice % bytes-consumed))
+                     (recur))))))))))
 
 (defn- start-nrepl-server! [port host]
   (let [server (net/createServer #(handle-nrepl-client %))]
@@ -168,13 +222,16 @@
                 (fn [message]
                   (let [response (edn/read-string (.toString message))
                         session-id (:session response)
-                        socket (get @nrepl-sessions session-id)]
+                        socket (or (get @nrepl-sessions session-id)
+                                   (when (and (not session-id)
+                                              (= 1 (count @nrepl-sockets)))
+                                     (first @nrepl-sockets)))]
+                    (js/console.log "browser -> nREPL:" (clj->js response))
                     (if socket
                       (send-bencode socket (clj->js response))
                       (js/console.error "nREPL: No socket for session" session-id)))))
            (.on ws "close" #(do (js/console.log "nREPL browser disconnected.")
                                 (reset! nrepl-ws-channel nil)))))
-    (print "start-ws-server!")
     true
     ))
 
